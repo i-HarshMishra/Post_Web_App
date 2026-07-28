25eba541233260affd1362cb05fb154155d7ea11
Harsh Mishra <harshmishra4dec@gmail.com>
2026-07-28 23:23:59 +0530

    live update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+      interval: 100
+    }
   }
 });
